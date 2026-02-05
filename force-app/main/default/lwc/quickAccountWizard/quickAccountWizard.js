706180a4c7ffd89d8deda1ab84a947c8705ea64d
WannabeYogi <nipun.jaipur@gmail.com>
2026-02-05 20:07:01 +0530

    soivnvoienvo

--- force-app/main/default/lwc/quickAccountWizard/quickAccountWizard.js
+++ force-app/main/default/lwc/quickAccountWizard/quickAccountWizard.js
@@ -26,7 +26,7 @@ export default class QuickAccountWizard extends LightningElement {
     }
 
     handleCreate() {
-        // Double check validation (Redundant but safe)
+        // Double check validation (Redundant but safe) efve
         if (this.isButtonDisabled) return;
 
         createAccount({ name: this.name, phone: this.phone, age: parseInt(this.age) })
